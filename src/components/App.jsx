5bc092ab08625ff4f8a88ff7f94639b99d0c8bea
Konrad Szwarc <konrad.szwarc@outlook.com>
2017-11-29 00:38:38 +0100

    Get drinks from API and display them on list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Searchbar from './Searchbar';
 import DrinksList from './DrinksList';
 import DrinkDetails from './DrinkDetails';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinks: [],
+    };
+  }
+
+  componentWillMount() {
+    axios.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+      .then((data) => {
+        const { drinks } = data.data;
+        this.setState({ drinks });
+      });
+  }
+
   render() {
     return (
       <div>
         <Searchbar />
         <div className="wrapper">
           <DrinkDetails />
-          <DrinksList />
+          <DrinksList drinks={this.state.drinks} />
         </div>
       </div>
     );
--- src/components/DrinksList.jsx
+++ src/components/DrinksList.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
 class DrinksList extends Component {
+  renderDrink(drink, index) {
+    if (index < 15) {
+      return <li key={index} className="list-group-item drink-item">{drink.strDrink}</li>;
+    }
+    return null;
+  }
+
   render() {
     return (
       <div style={{ width: '35%', marginTop: 30 }}>
         <ul className="list-group" style={{ width: '100%' }}>
-          <li className="list-group-item drink-item">3-Mile Long Island Iced Tea</li>
-          <li className="list-group-item drink-item">69 Special</li>
-          <li className="list-group-item drink-item">Abbey Cocktail</li>
-          <li className="list-group-item drink-item">Abbey Martini</li>
-          <li className="list-group-item drink-item">Adam & Eve</li>
+          {this.props.drinks.map(this.renderDrink)}
         </ul>
       </div>
     );
